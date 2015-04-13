5e5435bf71ad3a0c69ef578b9e473071164b3dd9
Christina Thompson <c.thompsonms@gmail.com>
2015-04-12 21:20:39 -0700

    created methods to check for project in project file, verify it exist, and find backers for project and backed amount

--- ks.rb
+++ ks.rb
@@ -6,7 +6,19 @@ require './read_write.rb'
 @project_file = File.new("projects.txt", "a")
 @backer_file = File.new("backers.txt", "a")
 
-def find_backers_for_project(project)
+def check_for_project_in_project_file(project_name)
+  ReadWrite.get_all_projects.select {|entry| entry["name"] == project_name}
+end
+
+def verify_project_exist(project)
+  num_of_matches = check_for_project_in_project_file(project).count
+
+  if (num_of_matches > 0)
+    return true
+  end
+end
+
+def find_backers_for_project_and_return_amt(project)
   # Iterate over @backed_proojects array to find backers that backed x project
   results = ReadWrite.get_all_backed_projects.select { |p| p["project_name"] == project }
 
@@ -60,41 +72,17 @@ elsif (ARGV[0] == "list")
     puts "Please provide a project name."
   else
 
-    # Check if project exist in projects.txt file // maybe not
-    matches = ReadWrite.get_all_projects.select {|entry| entry["name"] == ARGV[1]}
-    num_of_matches = matches.count
+    if (verify_project_exist(ARGV[1]))
+      backed_amt = find_backers_for_project_and_return_amt(ARGV[1])
       
-    if (num_of_matches > 0)
-      puts "do some stuff."
+      amt_needed = check_for_project_in_project_file(ARGV[1])[0]["target_amount"][1..-1].to_i - backed_amt
+
+      puts amt_needed == 0 ? ARGV[1] + " is successful!" : ARGV[1] + " needs $#{amt_needed} more dollars to be successful"
     else
       puts ARGV[1] + " does not exist."
     end
 
-    # Iterate over @backed_proojects array to find backers that backed x project
-    # backers_that_backed(ARGV[1])
-
-    # results = ReadWrite.get_all_backed_projects.select { |p| p["project_name"] == ARGV[1] }
-
-    # Iterate again to print backers name and amt
-    # backed_amt = 0
-
-    # if (results.count > 0)
-    #   results.each do |b|
-    #     puts b["name"] + " backed #{b["project_name"]} for $#{b["backing_amount"]}"
-    #     backed_amt = backed_amt + b["backing_amount"].to_i
-    #   end
-    # else
-    #   puts "Be the first to back this project."
-    # end
-
-    backed_amt = find_backers_for_project(ARGV[1])
-    
-    amt_needed = matches[0]["target_amount"][1..-1].to_i - backed_amt
-
-    puts amt_needed == 0 ? ARGV[1] + " is successful!" : ARGV[1] + " needs $#{amt_needed} more dollars to be successful"
-
   end
-  
 elsif (ARGV[0] == "backer")
   # Make sure input arg is passed in with 'backer' command
   if ARGV[1] == nil
