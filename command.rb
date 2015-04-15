7c9b2dc1442a776c478b8fc72ec9b0a1c02ebf0b
Christina Thompson <c.thompsonms@gmail.com>
2015-04-14 22:55:36 -0700

    print out backer for a specific project

--- command.rb
+++ command.rb
@@ -46,6 +46,9 @@ class Command
       project = Project.find_by_name(args[1]).first
 
       if (project)
+        Backer.find_all_backers_by_project_name(args[1]).each do |back|
+          puts "#{back["name"]} backed #{back["project_name"]} for #{back["backing_amount"]}"
+        end
         amt_needed = project["target_amount"].to_i - Backer.return_backed_amt_for_project(args[1])
 
         puts amt_needed == 0 ? args[1] + " is successful!" : args[1] + " needs $#{amt_needed} more dollars to be successful"
