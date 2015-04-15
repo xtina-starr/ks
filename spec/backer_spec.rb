2a482237b55e73e6f515dbba229eee047fd249b7
Christina Thompson <c.thompsonms@gmail.com>
2015-04-15 10:16:34 -0700

    test correct amount gets returned

--- spec/backer_spec.rb
+++ spec/backer_spec.rb
@@ -1,9 +1,22 @@
 require './backer'
+require 'json'
 
-describe Backer do 
-  it "does some stuff" do
-    mock_file = File.read("./spec/support/backers.txt")
-    allow(File).to receive(:open).with("backers.txt").and_return(mock_file)
-    expects(Backer).to eq
+describe Backer do
+  it "#find_all_project_by_backer should find all projects backed by a given user" do
+    backer_name = "Christina"
+
+    expect(Backer.find_all_project_by_backer(backer_name).count).to eq(8)
+  end
+
+  it "#return_backed_amt_for_project should calculate the correct backing total for a given project that has been backed" do
+    project = "Arts-Crafts"
+
+    expect(Backer.return_backed_amt_for_project(project)).to eq(150)
+  end
+
+  it "#find_all_backers_by_project_name should find projects by project name" do 
+    project_name = "Arts-Crafts"
+
+    expect(Backer.find_all_backers_by_project_name(project_name).count).to eq(2)
   end
 end
